303e24d9cf1da8ed7db3ae1713fd05925a08e3d4
mertyalcin-code <mert1943992@gmail.com>
2021-11-25 15:41:59 +0300

    make it better

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationGuard } from './guard/authentication.guard';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { RegisterSuccessComponent } from './register-success/register-success.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PoemManagementComponent } from 'src/app/poem-management/poem-management.component';
+import { UserManagementComponent } from 'src/app/user-management/user-management.component';
+import { AdminGuard } from './guard/admin.guard';
+import { EditorGuard } from './guard/editor.guard';
+import { UserComponent } from './user/user.component';
+import { MyAccountComponent } from './my-account/my-account.component';
+import { CategoryComponent } from './category/category.component';
+import { RandomPoemComponent } from './random-poem/random-poem.component';
+import { TestComponent } from './test/test.component';
+import { MostCommentComponent } from './populer/most-comment/most-comment.component';
+import { MostLikedComponent } from './populer/most-liked/most-liked.component';
+import { SearchPoemsComponent } from './search-poems/search-poems.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login',component:LoginComponent},
+  {path: 'register',component:RegisterComponent},
+  {path: 'register-success',component:RegisterSuccessComponent},
+  {path: 'test',component:TestComponent},
+  {path: 'home',component:DashboardComponent,canActivate: [AuthenticationGuard] },
+  {path: 'random',component:RandomPoemComponent,canActivate: [AuthenticationGuard] },
+  {path: 'most-comment',component:MostCommentComponent,canActivate: [AuthenticationGuard] },
+  {path: 'most-liked',component:MostLikedComponent,canActivate: [AuthenticationGuard] },
+  {path: 'poem/search',component:SearchPoemsComponent,canActivate: [AuthenticationGuard] },
+  {path: 'user/:username',component:UserComponent,canActivate: [AuthenticationGuard] },
+  {path: 'categories/:category',component:CategoryComponent,canActivate: [AuthenticationGuard] },
+  {path: 'myaccount',component:MyAccountComponent,canActivate: [AuthenticationGuard] },
+  {path: 'admin/user/management',component:UserManagementComponent,canActivate: [AdminGuard] }, 
+  {path: 'admin/poem/management',component:PoemManagementComponent,canActivate: [EditorGuard] }, 
+  {path: '', redirectTo : 'login',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,79 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthenticationService } from './service/authentication.service';
+import { UserService } from './service/user.service';
+import { AuthInterceptor } from './interceptor/auth.interceptor';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NotificationModule } from './notification.module';
+import { NotificationService } from './service/notification.service';
+import { AuthenticationGuard } from './guard/authentication.guard';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { RegisterComponent } from './register/register.component';
+import { RegisterSuccessComponent } from './register-success/register-success.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AngularEditorModule } from '@kolkov/angular-editor';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PoemManagementComponent } from 'src/app/poem-management/poem-management.component';
+import { UserManagementComponent } from './user-management/user-management.component';
+import { MyAccountComponent } from './my-account/my-account.component';
+import { CategoryComponent } from './category/category.component';
+import { RandomPoemComponent } from './random-poem/random-poem.component';
+import { TestComponent } from './test/test.component';
+import { PoemBoxComponent } from './poem-box/poem-box.component';
+import { MostLikedComponent } from './populer/most-liked/most-liked.component';
+import { MostCommentComponent } from './populer/most-comment/most-comment.component';
+import { SearchPoemsComponent } from './search-poems/search-poems.component';
+
+
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [																			
+    AppComponent,
+      LoginComponent,
+      UserComponent,
+      UserComponent,
+      RegisterComponent,
+      RegisterSuccessComponent,
+      FooterComponent,
+      NavbarComponent,        
+      UserComponent,
+      DashboardComponent,
+      PoemManagementComponent,
+      UserManagementComponent,
+      MyAccountComponent,
+      CategoryComponent,
+      RandomPoemComponent,
+      TestComponent,
+      PoemBoxComponent,
+      MostLikedComponent,
+      MostCommentComponent,
+      SearchPoemsComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NotificationModule,
+    ReactiveFormsModule,
+    AngularEditorModule,    
+
   ],
-  providers: [],
+  providers: [
+    AuthenticationService,
+    UserService,
+    {provide: HTTP_INTERCEPTORS,useClass:AuthInterceptor,multi:true},
+    AuthenticationGuard,
+    NotificationService
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
